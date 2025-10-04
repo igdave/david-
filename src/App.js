c37ad6ba44825b3ae6c3d8fed592fde5734b718b
igbah <igbahdavid@gmail.com>
2025-10-05 00:03:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Product from "./Components/Product";
+import About from "./Components/About";
+import News from "./Components/News";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import Services from "./Components/Services";
+import Shop from "./Components/Shop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is my App component</h1>
+      <Home />
+      <About />
+      <Product />
+      <Services />
+      <News />
+      <Contact />
+      <Shop />
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
